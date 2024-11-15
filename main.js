35f58330973826573de630b8b0217ac988b232aa
seyfire <97469939+Seyfire@users.noreply.github.com>
2024-11-15 16:16:06 -0500

    add functions to populate the display

--- main.js
+++ main.js
@@ -27,6 +27,28 @@ function operate(a, b, op) {
     }
 }
 
+function addToDisplay (str) {
+    // appends number to the display
+    display.textContent = display.textContent + str;
+}
+
+function updateDisplayNumber () {
+    // updates displayNumber to whatever is on the display
+    displayNumber = display.textContent; 
+}
+
+let display = document.querySelector(".display");
+
+// create eventListeners for the number buttons
+const nodeListNumber = document.querySelectorAll(".number");
+const numButtonsArr = Array.from(nodeListNumber).map( (button) => {
+    button.addEventListener("click", () => {
+        addToDisplay(button.textContent); 
+        updateDisplayNumber();
+    });
+});
+
 let number1 = 0;
 let number2 = 0;
+let displayNumber;
 let operator;
